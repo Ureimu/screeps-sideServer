cdb2437f86954ea14b466e2f70e54f8a3f34213b
Ureimu <steam1090693441@163.com>
2021-09-28 15:47:12 +0800

    更新authInfo模板文件

--- authInfoSample.ts
+++ authInfoSample.ts
@@ -1,5 +1,32 @@
 // authInfo模板文件，在使用test的时候会用到
-export const userData = {
-    email: "notMyEmail@abc.com",
-    password: "notMyPassword"
+import { ApiConfig } from "node-ts-screeps-api/dist/src/type";
+
+const data: { [name: string]: ApiConfig<"signinByPassword"> } = {
+    dev: {
+        authInfo: {
+            type: "signinByPassword",
+            email: "notMyEmail@abc.com",
+            password: "notMyPassword"
+        },
+        hostInfo: {
+            protocol: "https",
+            port: 443,
+            path: "/",
+            hostname: "screeps.com"
+        }
+    },
+    private: {
+        authInfo: {
+            type: "signinByPassword",
+            email: "notMyEmail@abc.com",
+            password: "notMyPassword"
+        },
+        hostInfo: {
+            protocol: "localhost",
+            port: 21025,
+            path: "/",
+            hostname: "127.0.0.1"
+        }
+    }
 };
+export const apiConfig = (dataType: string): ApiConfig<"signinByPassword"> => data[dataType];
